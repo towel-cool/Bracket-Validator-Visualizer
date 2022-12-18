1dfc603bd2b06cdc78173169140031dee6d860d5
towel-cool <daminecraft5@gmail.com>
2022-12-17 19:08:32 -0500

    Finished the validate bracket function

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const searchval = window.location.search;
 const urlParams = new URLSearchParams(searchval);
 
-function ensureBrackets(brackets = []) {
-    /* This function checks that the input is only brackets. 
-        Returns true or false*/
-    for (let i = 0; i < brackets.length; i++) {
-        if (brackets[i] != '(' || brackets[i] != ')' || brackets[i] != '{'
-            || brackets[i] != '}' || brackets[i] != '[' || brackets[i] != ']') {
+async function sleep(seconds) {
+    return new Promise(resolve=>setTimeout(resolve, seconds*1000));
+}
+
+async function validBrackets(sequence) {
+    /* This function checks that brackets are valid */
+    let stack = [];
+
+    for (let i = 0; i < sequence.length; i++) {
+        if (sequence[i] == '(' || sequence[i] == '{' || sequence[i] == '[') {
+            stack.push(sequence[i]);
+        }
+        else if (sequence[i] == ")") {
+            if (stack[stack.length - 1] != '(') {
                 return false;
             }
+            stack.pop();
+        }
+        else if (sequence[i] == '}') {
+            if (stack[stack.length - 1] != '{') {
+                return false;
+            }
+            stack.pop();
+        }
+        else if (sequence[i] == ']') {
+            if (stack[stack.length - 1] != '[') {
+                return false;
+            }
+            stack.pop();
+        }
+        await sleep(1.5);
+    }
+    if (stack.length > 0) {
+        return false;
     }
     return true;
 }
 
-function validateBrackets() {
-    /* This function checks that brackets are valid */
-    let sequence = urlParams.get("sequence");
-    let stack = [];
-    
-    for (let i = 0; i < sequence.length; i++) {
-        
-    }
-}
-
-validateBrackets();
+let sequence = urlParams.get("sequence");
+validBrackets(sequence);
